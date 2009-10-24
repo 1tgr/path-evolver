f25bfdf23431ceeab1cd2d42bf66faf4dffae7dd
Tim Robinson <tim.g.robinson@gmail.com>
2009-10-24 19:31:26 +0100

    Evolve a circle

--- Evolution.hs
+++ Evolution.hs
@@ -19,18 +19,18 @@ randomPopulation fitness count = sequence
                                $ replicate count 
                                $ individual >>= return . fitness
 
-mutatePopulation :: (Individual i, Show i, Ord f, Show f, RandomGen g) => (i -> Fitness i f) -> Int -> Population i f -> State g (Population i f)
+mutatePopulation :: (Individual i, Ord f, Show f, RandomGen g) => (i -> Fitness i f) -> Int -> Population i f -> State g (Population i f)
 mutatePopulation fitness generation population = do
     individuals' <- sequence $ replicate (count - 1) $ mutate fittestIndividual
     let population' = parMap rwhnf fitness individuals'
     return (fittest : population')
     where fittest @ (fittestIndividual, fittestScore) = maximumBy (\ (_, a) (_, b) -> compare a b) population
-          message = "Generation " ++ (show generation) ++ ": " ++ (show $ fittest)
+          message = "Generation " ++ (show generation) ++ ": " ++ (show $ fittestScore)
           count = trace message $ length population
 
-evolve :: (Individual i, Show i, Ord f, Show f, RandomGen g) => (i -> Fitness i f) -> State g (Fitness i f)
+evolve :: (Individual i, Ord f, Show f, RandomGen g) => (i -> Fitness i f) -> State g (Fitness i f)
 evolve fitness = do
-    initialPopulation <- randomPopulation fitness 100
+    initialPopulation <- randomPopulation fitness 10000
     finalPopulation <- foldM (flip ($)) initialPopulation 
                     $ zipWith (flip ($)) [ 1 .. 1000 ] 
                     $ repeat
--- Main.hs
+++ Main.hs
@@ -6,8 +6,8 @@ import Path.Evolver.Evolution
 import Random
 
 data Instruction = Forward
-                 | Left
-                 | Right
+                 | TurnLeft
+                 | TurnRight
                  deriving (Bounded, Enum, Show)
 
 data Mutation = None
@@ -18,10 +18,16 @@ data Mutation = None
 data Turtle = Turtle [ Instruction ]
               deriving (Show)
 
+collapse :: [ Instruction ] -> [ Instruction ]
+collapse (TurnLeft : TurnRight : xs) = collapse xs
+collapse (TurnRight : TurnLeft : xs) = collapse xs
+collapse (x : xs) = x : collapse xs
+collapse [ ] = [ ]
+
 instance Individual Turtle where
-    individual = (sequence $ replicate 10 $ State random) >>= return . Turtle
+    individual = (sequence $ replicate 10 $ State random) >>= return . Turtle . collapse
     mutate (Turtle instructions) =
-        mutateInstructions instructions >>= return . Turtle
+        mutateInstructions instructions >>= return . Turtle . collapse
         where mutateInstructions :: RandomGen g => [ Instruction ] -> State g [ Instruction ]
               mutateInstructions (x : xs) = do
                   mutation <- State random
@@ -41,8 +47,70 @@ instance Individual Turtle where
               applyMutation i Add = State random >>= (\i' -> return [ i, i' ])
               applyMutation _ Drop = return [ ]
 
-fitness :: Turtle -> Fitness Turtle Int
-fitness t = (t, 0)
+type Location = (Double, Double)
+
+data Direction = North
+               | East
+               | South
+               | West
+
+data TurtleState = TurtleState
+                 {
+                     location :: Location,
+                     direction :: Direction
+                 }
+
+path :: [ Instruction ] -> [ Location ]
+path = (++ [ (0, 0) ])
+     . ((0, 0) : )
+     . (flip evalState) (TurtleState { location = (0, 0), direction = North })
+     . mapM applyInstructionM
+    where applyInstruction :: Instruction -> TurtleState -> TurtleState
+          applyInstruction Forward state @ TurtleState { location = (x, y), direction = North } = state { location = (x, y - 1) }
+          applyInstruction Forward state @ TurtleState { location = (x, y), direction = East } = state { location = (x + 1, y) }
+          applyInstruction Forward state @ TurtleState { location = (x, y), direction = South } = state { location = (x, y + 1) }
+          applyInstruction Forward state @ TurtleState { location = (x, y), direction = West } = state { location = (x - 1, y) }
+          applyInstruction TurnLeft state @ TurtleState { direction = North } = state { direction = West }
+          applyInstruction TurnLeft state @ TurtleState { direction = East } = state { direction = North }
+          applyInstruction TurnLeft state @ TurtleState { direction = South } = state { direction = East }
+          applyInstruction TurnLeft state @ TurtleState { direction = West } = state { direction = South }
+          applyInstruction TurnRight state @ TurtleState { direction = North } = state { direction = East }
+          applyInstruction TurnRight state @ TurtleState { direction = East } = state { direction = South }
+          applyInstruction TurnRight state @ TurtleState { direction = South } = state { direction = West }
+          applyInstruction TurnRight state @ TurtleState { direction = West } = state { direction = North }
+
+          applyInstructionM :: Instruction -> State TurtleState Location
+          applyInstructionM instruction = do
+              state <- get
+              let state' @ TurtleState { location = l } = applyInstruction instruction state
+              put state'
+              return l
+
+centre :: [ Location ] -> Location
+centre p = (sumX / pathLength, sumY / pathLength)
+    where (sumX, sumY) = foldl1 (\(x1, y1) (x2, y2) -> (x1 + x2, y1 + y2)) p
+          pathLength = fromRational $ toRational $ length p
+
+stats :: (Floating a, Ord a) => [ a ] -> (a, a, a, a, a, a)
+stats (x:xs) = finish . foldl stats' (x, x, x, x*x, 1) $ xs
+  where stats' (mx, mn, s, ss, n) xx = (max xx mx, min xx mn, s + xx, ss + xx * xx, n+1)
+        finish (mx, mn, s, ss, n) = (mx, mn, av, va, stdev, n)
+          where av = s/n
+                va = ss/(n-1) - n*av*av/(n-1)
+                stdev = sqrt va
+
+stats [ ] = error "stats: empty list"
+
+fitness :: Turtle -> Fitness Turtle (Double, Double, Double)
+fitness turtle @ (Turtle instructions) =
+    let p = path instructions
+        square x = x * x
+        (centreX, centreY) = centre p
+        (_, _, av, _, stdev, _) = stats
+                                $ map (\(x, y) -> square (x - centreX) + square (y - centreY))
+                                $ p
+        score = av / (1 + stdev)
+    in (turtle, (score, av, stdev))
 
 main :: IO ()
 main =
