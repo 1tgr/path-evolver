f100634bdb206f77b72e1998a37afb78bfe81c57
Tim Robinson <tim.g.robinson@gmail.com>
2009-10-26 20:57:27 +0000

    OpenGL window shows progress

--- Main.hs
+++ Main.hs
@@ -7,28 +7,25 @@ import Path.Evolver.Evolution
 import Path.Evolver.Turtle  
 import Random
 
-displayPath :: [ Location ] -> IO ()
-displayPath ((x1, y1) : (p @ ((x2, y2) : _))) = do
-    vertex $ Vertex2 x1 y1
-    vertex $ Vertex2 x2 y2
-    displayPath p
-
-displayPath _ = return ()
+fittest :: Ord f => Population Turtle f -> Turtle
+fittest = fst . maximumBy (\ (_, a) (_, b) -> compare a b)
 
 display :: Turtle -> IO ()
 display (Turtle instructions) = do
   	clear [ ColorBuffer, DepthBuffer ]
   	loadIdentity
-  	renderPrimitive Lines $ displayPath $ path instructions
+  	renderPrimitive LineStrip
+  	    $ mapM_ (\(x, y) -> vertex $ Vertex2 x y)
+  	    $ path
+  	    $ instructions
   	flush
 
-oneGeneration :: (Ord f, RandomGen g) => (Population Turtle f -> State g (Population Turtle f)) -> Population Turtle f -> g -> IO (Population Turtle f, g)
-oneGeneration mutateFunc population g = do
-    let ret @ (population', _) = runState (mutateFunc population) g
-    let fittest = fst $ maximumBy (\ (_, a) (_, b) -> compare a b) $ population'
-    displayCallback $= display fittest
+oneGeneration :: (Ord f, RandomGen g) => (Int -> Population Turtle f -> State g (Population Turtle f)) -> Int -> Population Turtle f -> g -> IO ()
+oneGeneration mutateFunc generation population g = do
+    let (population', g') = runState (mutateFunc generation population) g
+    displayCallback $= (display $ fittest population')
+    idleCallback $= Just (oneGeneration mutateFunc (generation + 1) population' g')
     postRedisplay Nothing
-    return ret
 
 main :: IO ()
 main = do
@@ -38,11 +35,9 @@ main = do
     clearColor $= Color4 0 0 0 0
     matrixMode $= Projection
     loadIdentity
-    ortho (-1) 1 (-1) 1 (-1) 1
+    ortho2D (-50) 50 (-50) 50
     matrixMode $= Modelview 0
-    foldM (\(p, g) f -> f p g) (runState (randomPopulation fitness 10000) $ mkStdGen 0)
-        $ map oneGeneration
-        $ zipWith (flip ($)) [ 1 .. 1000 ] 
-        $ repeat
-        $ mutatePopulation fitness
+    let (population, g) = runState (randomPopulation fitness 10000) $ mkStdGen 0
+    displayCallback $= (display $ fittest population)
+    idleCallback $= Just (oneGeneration (mutatePopulation fitness) 1 population g)
     mainLoop
